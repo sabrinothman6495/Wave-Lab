4cbe2697846c5b1fc77e574b99a4d901abaacb50
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-12-28 14:00:05 -0500

    updated config file to correcr port

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3001,
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/soundboard',
   jwtSecret: process.env.JWT_SECRET || 'soundboard',
   env: process.env.NODE_ENV || 'development',
