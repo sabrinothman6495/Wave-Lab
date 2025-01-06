3bd80e5a96e6eb603e98acb6909bf275d2b3ecc5
Graceatkirk <grace@kirk.org>
2025-01-06 17:50:16 -0600

    updated stuff on the front end for login and sound info

--- Wave-Lab/src/utils/Queries.ts
+++ Wave-Lab/src/utils/Queries.ts
@@ -1,21 +1,5 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  query user($email: String!) {
-    user(email: $email) {
-      _id
-      firstName
-      lastName
-      email
-      sound {
-        _id
-        audioUrl
-        soundTitle
-      }
-    }
-  }
-`;
-
 export const QUERY_ME = gql`
   query me {
     me {
@@ -27,40 +11,7 @@ export const QUERY_ME = gql`
         _id
         audioUrl
         soundTitle
-        instrument {
-          _id
-          piano
-          guitar
-          trumpet
-        }
       }
-    }
-  }
-`;
-
-export const GET_USER_PROFILE = gql`
-  query GetUserProfile {
-    me {
-      _id
-      firstName
-      lastName
-      email
-      sound {
-        _id
-        soundTitle
-        audioUrl
-      }
-    }
-  }
-`;
-
-export const UPDATE_PROFILE = gql`
-  mutation UpdateProfile($firstName: String, $lastName: String, $email: String) {
-    updateUser(firstName: $firstName, lastName: $lastName, email: $email) {
-      _id
-      firstName
-      lastName
-      email
     }
   }
 `;
