86aa791942bee1a73aa4873ef96b3dbfd620e816
Andrew <mrangus298@gmail.com>
2025-01-05 18:50:46 -0600

    Added functionality to the nav

--- Wave-Lab/src/pages/homePage.tsx
+++ Wave-Lab/src/pages/homePage.tsx
@@ -3,14 +3,14 @@ import NavBar from "../components/navbar/navbar";
 import Piano from "../components/piano/piano";
 import Title from "../components/navbar/title";
 
-const HomePage: React.FC = () => {
+const homePage: React.FC = () => {
     return (
         <div>
             <Title logo="/logo192.png" title="Wave Lab" />
-            <NavBar profile="/profile" home="/home" landingPage="/" />
+            <NavBar profilePage="/profile" homePage="/home" landingPage="/" />
             <Piano />
         </div>
     );
 };
 
-export default HomePage;
+export default homePage;
