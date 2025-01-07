753b7b033ebdd713713b666c1e4ce72622808ce7
Graceatkirk <grace@kirk.org>
2025-01-07 17:20:23 -0600

    lil commit

--- Wave-Lab/vite.config.ts
+++ Wave-Lab/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()] as const,  // Add type assertion here
+  plugins: [react()],  // Add type assertion here
   server: {
     port: 5174, // vite dev server port
     proxy: {
