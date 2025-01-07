ff8b7f30cdfcd25e16897769da7e5ad9485bc7c1
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2025-01-07 12:15:55 -0500

    minor update to cleanDB so that we console log any db issues

--- server/src/seeds/cleanDB.ts
+++ server/src/seeds/cleanDB.ts
@@ -2,12 +2,26 @@ import Sound from '../models/Sound';
 import User from '../models/user';
 import process from 'process';
 
-async function cleanDB() {
-  await Sound.deleteMany({});
-  await User.deleteMany({});
-  process.exit(0);
-}
+// async function cleanDB() {
+//   await Sound.deleteMany({});
+//   await User.deleteMany({});
+//   process.exit(0);
+// }
 
-cleanDB();
+// cleanDB();
+
+// export default cleanDB;
+
+const cleanDB = async () => {
+  try {
+    await Sound.deleteMany({});
+    await User.deleteMany({});
+    console.log('Database cleaned.');
+    process.exit(0);
+  } catch (error) {
+    console.error('Database cleaning error:', error);
+    process.exit(1);
+  }
+}
 
 export default cleanDB;
