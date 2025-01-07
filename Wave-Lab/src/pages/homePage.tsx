bc48f9d49dbc1aeaf23eb1c5d72cff596c0b6871
Hannahliz0 <hnbright57@gmail.com>
2025-01-06 20:28:18 -0600

    homepage i think done?? need confirmation

--- Wave-Lab/src/pages/homePage.tsx
+++ Wave-Lab/src/pages/homePage.tsx
@@ -1,20 +1,83 @@
-import React from "react";
-import NavBar from "../components/navbar/navbar";
+import React, { useState } from "react";
 import Piano from "../components/piano/piano";
-import Title from "../components/navbar/title";
+import "./homePage.css";
 
-const homePage: React.FC = () => {
+const HomePage: React.FC = () => {
+    const [isInstrumentsOpen, setIsInstrumentsOpen] = useState(false);
+    const [isLevelsOpen, setIsLevelsOpen] = useState(false);
+
+    const toggleInstruments = () => {
+        setIsInstrumentsOpen(!isInstrumentsOpen);
+        if (!isInstrumentsOpen) setIsLevelsOpen(false); 
+    };
+
+    const toggleLevels = () => {
+        setIsLevelsOpen(!isLevelsOpen);
+        if (!isLevelsOpen) setIsInstrumentsOpen(false); 
+    };
+
     return (
-        <div>
-<<<<<<<<< Temporary merge branch 1
-            <Title logo="/logo192.png" title="Wave Lab" />
-            <NavBar profilePage="/profile" homePage="/home" landingPage="/" />
-=========
-            <NavBar profile="/profile" home="/home" landingPage="/landingPage" />
->>>>>>>>> Temporary merge branch 2
-            <Piano />
+        <div className="homepage-container">
+            {/* Navbar */}
+            <div className="navbar">
+                <h1 className="title">Music Maker</h1>
+            </div>
+
+            {/* Content Section */}
+            <div className="content">
+                {/* Dropdowns */}
+                <div className="dropdown-container">
+                    <div className="dropdown">
+                        <button className="dropbtn" onClick={toggleInstruments}>
+                            Instruments
+                        </button>
+                        {isInstrumentsOpen && (
+                            <div className="dropdown-content dropdown-content-instrument">
+                                <label>
+                                    <input type="checkbox" name="piano" /> Piano
+                                </label>
+                                <label>
+                                    <input type="checkbox" name="guitar" /> Guitar
+                                </label>
+                                <label>
+                                    <input type="checkbox" name="violin" /> Violin
+                                </label>
+                                <label>
+                                    <input type="checkbox" name="flute" /> Flute
+                                </label>
+                            </div>
+                        )}
+                    </div>
+                    <div className="dropdown">
+                        <button className="dropbtn" onClick={toggleLevels}>
+                            Levels
+                        </button>
+                        {isLevelsOpen && (
+                            <div className="dropdown-content dropdown-content-levels">
+                                <label>
+                                    Bass:
+                                    <input type="range" min="0" max="100" />
+                                </label>
+                                <label>
+                                    Treble:
+                                    <input type="range" min="0" max="100" />
+                                </label>
+                                <label>
+                                    Mid:
+                                    <input type="range" min="0" max="100" />
+                                </label>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                {/* Piano Section */}
+                <div className="piano-section">
+                    <Piano />
+                </div>
+            </div>
         </div>
     );
 };
 
-export default homePage;
+export default HomePage;
